ab861429906e239ae03163858df57e0df96a3dd6
ghostdp <new666@qq.com>
2016-05-05 14:39:29 +0800

    Added files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var concat = require('gulp-concat');
 var amdOptimize = require("amd-optimize");  
 var babel = require("gulp-babel");
 var less = require('gulp-less');
-//npm install --save-dev gulp-babel babel-preset-es2015
 
 gulp.task('default',['lessTask','rjsConcat'], function() {
   	console.log('gulp任务');
@@ -16,19 +15,19 @@ gulp.task('concat', function() {
 	gulp.src('src/*.js').pipe(concat('c.js')).pipe(gulp.dest('dist'));
 });
 
-gulp.task('rjsConcat',['babelTask'], function() {
+gulp.task('rjsConcat', function() {
   	console.log('gulp之requireJS合并任务');
-	gulp.src('dist/module_*.js').pipe(amdOptimize("module_c")).pipe(concat('module_c.js')).pipe(gulp.dest('dist'));
+	gulp.src('src/es5/module_*.js').pipe(amdOptimize("module_c")).pipe(concat('module_c.js')).pipe(gulp.dest('dist'));
 });
 
-gulp.task('babelTask' , function() {
+gulp.task('babelTask',function() {
   	console.log('es6转成es5');
 	gulp.src('src/module_*.js').pipe(babel({
         presets: ['es2015']
-    })).pipe(gulp.dest('dist'));
+    })).pipe(gulp.dest('src/es5'));
 });
 
-gulp.task('lessTask' , function() {
+gulp.task('lessTask',function() {
   	console.log('less转成css');
 	gulp.src('src/style.less').pipe(less()).pipe(gulp.dest('dist'));
 });
